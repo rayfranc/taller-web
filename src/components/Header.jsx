a0fde07aac7a893b8c700b0b1f9770c419d560a6
Adrian <adriangomezceruto1@gamil.com>
2023-06-28 23:18:08 -0400

    Nav Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import "@fontsource/saira-condensed/"
+import './Header.css';
+import { NavLink } from 'react-router-dom';
 
-import {AppBar, Container, Button, Toolbar,Typography, Box, MenuItem, IconButton, Menu} from '@mui/material';
-import {Menu as MenuIcon}from '@mui/icons-material';
+import { AppBar, Container, Button, Toolbar, Typography, Box, MenuItem, IconButton, Menu } from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
 
 
 
-function Header({themeHandler,mode}){
-    const pages = ['Services', 'Tires', 'Fleet Services','Financing', 'Coupons','Customer Reviews', 'Contact Us'];
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
+function Header({ themeHandler, mode }) {
+  const pages = ['Home', 'Services', 'Tires', 'Fleet Services', 'Financing', 'Coupons', 'Customer Reviews', 'Contact Us'];
+  const links = ['/', '/wheel-alignment', '/tires', '/fleet-services', '/financing', '/coupons', '/customer-reviews', '/contact-us'];
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -19,19 +22,19 @@ function Header({themeHandler,mode}){
   };
 
 
-    return(
+  return (
     <AppBar position='absolute'>
-<Container >
-<Toolbar disableGutters>
-        <img style={{'height':'10vh'}}
-        src={'/bcity-logo-removebg-preview.png'}
-        srcSet={`/bcity-logo-removebg-preview.png`}
-        alt='Bcity-logo'
-        loading="lazy"
-      />
-          
-          <span style={{ display: { xs: 'flex', md: 'none' }, mr: 1, flexGrow:1 }} />  
-        
+      <Container >
+        <Toolbar disableGutters>
+          <img style={{ 'height': '10vh' }}
+            src={'/bcity-logo-removebg-preview.png'}
+            srcSet={`/bcity-logo-removebg-preview.png`}
+            alt='Bcity-logo'
+            loading="lazy"
+          />
+
+          <span style={{ display: { xs: 'flex', md: 'none' }, mr: 1, flexGrow: 1 }} />
+
           <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
@@ -61,30 +64,52 @@ function Header({themeHandler,mode}){
                 display: { xs: 'block', md: 'none' },
               }}
             >
-              {pages.map((page) => (
+              {/* {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+                  <Typography textAlign="center" >{page}</Typography>
                 </MenuItem>
-              ))}
+              ))} */}
+              {
+                pages.map((page) => (
+                  <MenuItem
+                    key={page}>
+                    <NavLink key={page} className={({ isActive }) => `${isActive ? 'active' : 'navLink'}`} to={links[pages.indexOf(page)]}>
+                      <Button
+                        sx={{ color: 'white', display: 'block', fontFamily: 'Saira Condensed', fontWeight: 600, fontSize: 15 }}>
+                        {page}
+                      </Button>
+                    </NavLink>
+                  </MenuItem>
+                ))}
             </Menu>
           </Box>
-          <Box sx={{flexGrow:1, display:{xs:'none', md:'flex'}}}></Box>
-          <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' }  }}>
-            {pages.map((page) => (
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
+          <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+            {/* {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block', fontFamily:'Saira Condensed', fontWeight:600,fontSize:15 }}
+                sx={{ my: 2, color: 'white', display: 'block', fontFamily: 'Saira Condensed', fontWeight: 600, fontSize: 15 }}
               >
                 {page}
               </Button>
-            ))}
-             
+            ))} */}
+            {
+              pages.map((page) => (
+                <NavLink key={page} className={({ isActive }) => `${isActive ? 'active' : 'navLink'}`} to={links[pages.indexOf(page)]}>
+                  <Button
+                    key={page}
+                    sx={{ my: 2, color: 'white', display: 'block', fontFamily: 'Saira Condensed', fontWeight: 600, fontSize: 15 }}>
+                    {page}
+                  </Button>
+                </NavLink>
+              ))}
+
           </Box>
-</Toolbar>
-</Container>
+        </Toolbar>
+      </Container>
     </AppBar>
-    )
+  )
 }
 
 export default Header
